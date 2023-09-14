d1181a636660b5042af7898b08523cb8a1a4dd10
Khanh Bui <kbuiblue@gmail.com>
2023-09-15 00:20:16 +0700

    Implement booksRender and basic styling

---
+++ assignment-1/booksData.js
@@ -0,0 +1,32 @@
+export const booksData = [
+    {
+        name: "Design Patterns: Elements of Reusable Object-Oriented Software",
+        author: "Erich Gamma, Richard Helm, Ralph Johnson, John Vlissides, Grady Booch (Foreword)",
+        topic: "Design patterns, software engineering, object-oriented programming",
+        isDeleted: false,
+    },
+    {
+        name: "The Pragmatic Programmer: From Journeyman to Master",
+        author: "Andrew Hunt, David Thomas",
+        topic: "Education, computer programming",
+        isDeleted: false,
+    },
+    {
+        name: "Clean Code: A Handbook of Agile Software Craftsmanship",
+        author: "Robert Cecil Martin",
+        topic: "Software design, software architechture",
+        isDeleted: false,
+    },
+    {
+        name: "Cracking the Coding Interview",
+        author: "Gayle Laakmann McDowell",
+        topic: "Coding interview",
+        isDeleted: false,
+    },
+    {
+        name: "Refactoring: Improving the Design of Existing Code",
+        author: "Martin Fowler",
+        topic: "Code refactoring",
+        isDeleted: false,
+    },
+];
--- assignment-1/script.js
+++ assignment-1/script.js
@@ -1,1 +1,19 @@
-// Your JS code goes here
+import { booksData } from "./booksData";
+
+function renderBooks() {
+    const tbody = document.querySelector("tbody");
+
+    booksData.forEach((book) => {
+        const newRow = document.createElement("tr");
+        newRow.innerHTML = `
+            <td>${book.name}</td>
+            <td>${book.author}</td>
+            <td>${book.topic}</td>
+            <td class="delete-button">Delete</td>
+        `;
+
+        tbody.appendChild(newRow);
+    });
+}
+
+renderBooks();
