8d77c178f018bf073ec045f40beedf4c464c9f9b
Khanh Bui <kbuiblue@gmail.com>
2023-09-20 16:51:15 +0700

    Setting up Assignment 2

---
+++ assignment-2/src/components/App.js
@@ -0,0 +1,13 @@
+import styles from "../styles/App.module.css"
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <header className={styles.Header}>
+          <p className={styles.Link}>Test</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
