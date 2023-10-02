6c7a2accc7e0ef8aecdce30ad1b9f8a8ba1e1d13
Khanh Bui <kbuiblue@gmail.com>
2023-10-02 11:11:08 +0700

    Refactor index.tsx and move index.css

--- assignment-3/src/index.tsx
+++ assignment-3/src/index.tsx
@@ -1,24 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import './styles/index.css'
 import App from './components/App'
 import reportWebVitals from './reportWebVitals'
 import { ThemeProvider } from './components/context/ThemeProvider'
 
-const rootElement = document.getElementById('root')
+const rootElement = document.getElementById('root') as Element | DocumentFragment;
 
-
-if (rootElement) {
-    const root = ReactDOM.createRoot(rootElement)
-    root.render(
-        <React.StrictMode>
-            <ThemeProvider>
-                <App />
-            </ThemeProvider>
-        </React.StrictMode>,
-    )
-    // If you want to start measuring performance in your app, pass a function
-    // to log results (for example: reportWebVitals(console.log))
-    // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-    reportWebVitals()
-}
+const root = ReactDOM.createRoot(rootElement)
+root.render(
+    <React.StrictMode>
+        <ThemeProvider>
+            <App />
+        </ThemeProvider>
+    </React.StrictMode>,
+)
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
