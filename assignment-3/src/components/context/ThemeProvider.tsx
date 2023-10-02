e075d647f5c0ab654cacf60eb69ade58b18815c8
Khanh Bui <kbuiblue@gmail.com>
2023-10-02 11:20:21 +0700

    Debug ThemeProvider

--- assignment-3/src/components/context/ThemeProvider.tsx
+++ assignment-3/src/components/context/ThemeProvider.tsx
@@ -1,4 +1,10 @@
-import { ReactNode, useCallback, useState, useMemo, ChangeEventHandler } from 'react'
+import {
+    ReactNode,
+    useCallback,
+    useState,
+    useMemo,
+    ChangeEventHandler,
+} from 'react'
 import { ThemeContext } from './context'
 import { Theme } from '../types'
 
@@ -9,27 +15,28 @@ export interface ThemeProviderProps {
 export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
     const defaultTheme: Theme = JSON.parse(
         localStorage.getItem('defaultTheme') ||
-            "{ isChecked: false, type: 'light' }",
+            '{ "isChecked": false, "type": "light" }',
     )
     const [theme, setTheme] = useState(defaultTheme)
 
-    const handleSettingTheme: ChangeEventHandler<HTMLInputElement> = useCallback((event) => {
-        const isDark: boolean = event.target.checked
+    const handleSettingTheme: ChangeEventHandler<HTMLInputElement> =
+        useCallback((event) => {
+            const isDark: boolean = event.target.checked
 
-        if (isDark) {
-            setTheme({ isChecked: true, type: 'dark' })
-            localStorage.setItem(
-                'defaultTheme',
-                JSON.stringify({ isChecked: true, type: 'dark' }),
-            )
-        } else {
-            setTheme({ isChecked: false, type: 'light' })
-            localStorage.setItem(
-                'defaultTheme',
-                JSON.stringify({ isChecked: false, type: 'light' }),
-            )
-        }
-    }, [])
+            if (isDark) {
+                setTheme({ isChecked: true, type: 'dark' })
+                localStorage.setItem(
+                    'defaultTheme',
+                    JSON.stringify({ isChecked: true, type: 'dark' }),
+                )
+            } else {
+                setTheme({ isChecked: false, type: 'light' })
+                localStorage.setItem(
+                    'defaultTheme',
+                    JSON.stringify({ isChecked: false, type: 'light' }),
+                )
+            }
+        }, [])
 
     const value = useMemo(
         () => ({ theme, handleSettingTheme }),
